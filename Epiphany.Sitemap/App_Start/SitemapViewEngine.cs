e6b6f1377a0c52ccda4d9c026c358cf517f11ab3
Anthony Woodward <two.woods@gmail.com>
2017-01-05 22:39:30 +0000

    Custom Razor view engine which looks for views in "~/bin/Views/**".

---
+++ Epiphany.Sitemap/App_Start/SitemapViewEngine.cs
@@ -0,0 +1,25 @@
+﻿// Filename: SitemapViewEngine.cs
+// Author: Anthony Woodward
+// Created: 21:56 05-01-2017
+
+namespace Epiphany.Sitemap
+{
+    using System.Web.Mvc;
+
+    public class SitemapViewEngine : RazorViewEngine
+    {
+        public SitemapViewEngine()
+        {
+            MasterLocationFormats = new[]
+            {
+                "~/bin/Views/{1}/{0}.cshtml",
+                "~/bin/Views/Shared/{0}.cshtml"
+            };
+            ViewLocationFormats = new[]
+            {
+                "~/bin/Areas/{2}/Views/{1}/{0}.cshtml",
+                "~/bin/Areas/{2}/Views/Shared/{0}.cshtml"
+            };
+        }
+    }
+}
